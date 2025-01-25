26af6761b2d6f78194a487270f05e274bbef1993
Anirudh N Kamath <anirudhnkamath@Anirudhs-MacBook-Air-2.local>
2025-01-25 13:14:04 +0530

    added simple buttons

---
+++ frontend/frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <button>NODE</button>
+      <button>PYTHON</button>
+    </div>
+  );
+}
+
+export default App;
