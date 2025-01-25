3ff6a93fefd9d263a89f854af95ef828ef67c57c
Anirudh N Kamath <anirudhnkamath@Anirudhs-MacBook-Air-2.local>
2025-01-25 15:52:49 +0530

    user login and reg

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ import dotenv from 'dotenv';
 import app from './app.js'; 
 import { buildImages } from './services/dockerImage.js';
 import { dockerfileDirs } from './utils/dockerFilePath.js';
+import connectDatabase from './config/mongooseConfig.js';
+import usersRoute from './routes/userRoutes.js'
 
 dotenv.config();
 const port = process.env.PORT || 5000;
@@ -11,6 +13,10 @@ buildImages(dockerfileDirs);
 
 app.use(cors());
 app.use(express.json());
+
+connectDatabase();
+
+app.use("/users", usersRoute);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
